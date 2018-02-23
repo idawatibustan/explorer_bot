36fce449061728d653ece92a20fb992adc04efc0
idawatibustan <idawati.bustan@gmail.com>
2018-02-24 02:24:35 +0800

    Edit rosservice server for class object

--- src/bot_navigator.cpp
+++ src/bot_navigator.cpp
@@ -1,13 +1,15 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <nav_msgs/Odometry.h>
+#include <std_srvs/Empty.h>
+
 
 #include <iostream>
 #include <tf/tfMessage.h>
 #include <tf/transform_datatypes.h>
 #include <cmath>
 
-class BotController{
+class BotNavigator{
     private:
         ros::Subscriber pos_sub;
         ros::Publisher vel_pub;
@@ -15,17 +17,18 @@ class BotController{
         ros::ServiceServer move_y;
         double trans_x, trans_z;
         double pos_x, pos_y, ori_z, ang_z;
+        double ang_n, ang_e, ang_s, ang_w;
         double target_x, target_y, target_o, target_r;
         double init_r, init_ang_z, init_x, init_y;
 
     public:
-        BotController(ros::NodeHandle &nh){
+        BotNavigator(ros::NodeHandle &nh){
             trans_x = 0;
             trans_z = 0;
-            pos_sub = nh.subscribe("/odom",1,&BotController::callback, this);
+            pos_sub = nh.subscribe("/odom",1,&BotNavigator::callback, this);
             vel_pub = nh.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity",1);
-            ros::ServiceServer move_x = nh.advertiseService("move_x", move_x_callback);
-            ros::ServiceServer move_y = nh.advertiseService("move_y", move_y_callback);
+            move_x = nh.advertiseService("move_x", &BotNavigator::move_x_callback, this);
+            move_y = nh.advertiseService("move_y", &BotNavigator::move_y_callback, this);
         }
 
         bool move_x_callback( std_srvs::Empty::Request& req, std_srvs::Empty::Response& res )
@@ -79,9 +82,9 @@ class BotController{
 
             // send speed instruction, turning before translating
             base_cmd.linear.x = trans_x;
-            if(turn == 1){
-              base_cmd.angular.z = trans_z;
-            }
+            // if(turn == 1){
+            //   base_cmd.angular.z = trans_z;
+            // }
 
             vel_pub.publish(base_cmd);
             std::cout<< std::setprecision(2) << std::fixed;
@@ -96,7 +99,7 @@ class BotController{
 int main(int argc, char** argv){
     ros::init(argc, argv, "bot_navigator");
     ros::NodeHandle nh;
-    BotController bc(nh);
+    BotNavigator bn(nh);
 
     ros::spin();
     return 0;
