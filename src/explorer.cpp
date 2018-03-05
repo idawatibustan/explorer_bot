72a5c56937c15ff524cf4b33e8ae72f95ec8ef85
idawatibustan <idawati.bustan@gmail.com>
2018-03-05 23:18:39 +0800

    Update map_graph classes

--- src/explorer.cpp
+++ src/explorer.cpp
@@ -9,86 +9,75 @@
 #include <std_msgs/String.h>
 #include <nav_msgs/Odometry.h>
 
-class Position;
-class Node;
-// class Edge;
-
 class Position{
 private:
-  int x, y;
+  int x_, y_;
 public:
   Position(int x, int y){
-    this->x = x; this->y = y;
+    this->x_ = x; this->y_ = y;
   }
-  int getX() { return x; }
-  int getY() { return y; }
+  int getX() { return this->x_; }
+  int getY() { return this->y_; }
   double getDistance(Position other){
-    double x = (double) ( this->x - other.getX() );
-    double y = (double) ( this->y - other.getY() );
-    return sqrt( pow(x, 2) + pow(y, 2) );
+    double dx = this->x_ - other.getX();
+    double dy = this->y_ - other.getY();
+    return sqrt( pow(dx, 2) + pow(dy, 2) );
   }
 };
 
 class Node
 {
 private:
-  int id;
-  Position p;
-  double g, h, f;
-  bool calculated=false;
+  int id_;
+  Position p_;
+  double g_, h_, f_;
+  bool calculated;
 public:
-  Node(int id, int x, int y, const Position &goal) : p(x, y) {
-    this->id = id;
-    this->h = this->p.getDistance(goal);
+  Node(int id, int x, int y, const Position &goal) : p_(x, y) {
+    this->id_ = id;
+    this->h_ = this->p_.getDistance(goal);
+    this->calculated = false;
   }
-  Node(int id, const Position &p, const Position &goal) : p(p) {
-    this->id = id;
-    this->h = this->p.getDistance(goal);
+  Node(int id, const Position &p, const Position &goal) : p_(p) {
+    this->id_ = id;
+    this->h_ = this->p_.getDistance(goal);
+    this->calculated = false;
   }
-  double getPosX() { return p.getX(); }
-  double getPosY() { return p.getY(); }
-  double getH() { return h; }
+  double getPosX() { return this->p_.getX(); }
+  double getPosY() { return this->p_.getY(); }
+  int getId() { return this->id_; }
+  double getH() { return this->h_; }
   double getF() {
-    if (calculated)
-    return g+h;
-    else
-    return 0.0;
+    if (this->calculated) {
+      return this->f_;
+    }
+    else {
+      this->f_ = this->g_ + this->h_;
+      this->calculated = true;
+      return this->f_;
+    }
   }
-  double setG(double g) {
-    this->g = g;
+  double getG() { return this->g_; }
+  void setG(double g) {
+    this->g_ = g;
+    this->calculated = false;
   }
 };
 
-// class Edge{
-// private:
-//   bool checked;
-//   Node node_a, node_b;
-// public:
-//   std::vector<Node> nodes;
-//   Edge(const Node &a, const Node &b)
-//   : node_a(a), node_b(b) {
-//     this->checked = false;
-//   }
-//   Node getA() { return node_a; }
-//   Node getB() { return node_b; }
-//   bool get_checked() { return checked; }
-//   void set_checked() { checked = true; }
-// };
-
 class Graph{
 private:
-  std::map<int, std::vector<int>> adj;
+  std::map< int, std::vector<int> > adj;
   int node_count;
   int edge_count;
 public:
   Graph(int node_num){
-    node_count=node_num;
-    edge_count=0;
-  };
+    this->node_count=node_num;
+    this->edge_count=0;
+  }
   bool isEdgeValid(int a, int b){
-    bool isBinA = (std::find(adj[a].begin(), adj[a].end(), b) != adj[a].end());
-    bool isAinB = (std::find(adj[b].begin(), adj[b].end(), a) != adj[b].end());
-    return !isBinA && !isAinB;
+    bool b_in_a = (std::find(adj[a].begin(), adj[a].end(), b) != adj[a].end());
+    bool a_in_b = (std::find(adj[b].begin(), adj[b].end(), a) != adj[b].end());
+    return !b_in_a && !a_in_b;
   }
   void addEdge(int a, int b){
     if(isEdgeValid(a, b)) {
@@ -97,13 +86,12 @@ public:
       adj[b].push_back(a);
     }
   }
-  bool removeEdge(int a, int b){
+  void removeEdge(int a, int b){
+    std::cout << "removing " << a << "," << b << std::endl;
     edge_count--;
     adj[a].erase(std::remove(adj[a].begin(), adj[a].end(), b), adj[a].end());
     adj[b].erase(std::remove(adj[b].begin(), adj[b].end(), a), adj[b].end());
-  }
-  std::vector<int> getEdges(int n){
-    return adj[n];
+    return;
   }
   void printNeighbors(int n) {
     std::cout << '[' << n << "] ";
@@ -116,69 +104,133 @@ public:
     for(int i = 0; i < node_count; i++){
       printNeighbors(i);
     }
+    std::cout << "nodes: " << node_count
+    << ", edges:" << edge_count << std::endl;
   }
+  std::vector<int> getEdges(int n) { return adj[n]; }
+  int getNodeCount() { return node_count; }
+  int getEdgeCount() { return edge_count; }
 };
 
 class Map{
 private:
-  int size;
-  Position goal, init, curr;
+  int size_;
+  int id_goal, id_init, id_curr;
+  Position goal_, init_, curr_;
   Graph graph;
+  bool is_generated;
+  std::map<int, Node*> nodes;
+  std::vector<int> path;
 public:
-  std::vector<Node> nodes;
   Map(int size, const Position &goal, const Position &init)
-  : goal(goal), init(init), curr(init) {
-    this->size = size;
+  : goal_(goal), init_(init), curr_(init), graph(size*size) {
+    this->size_ = size;
+    this->id_goal = size * this->goal_.getX() + this->goal_.getY();
+    this->id_init = size * this->init_.getX() + this->init_.getY();
+    this->id_curr = size * this->curr_.getX() + this->curr_.getY();
+    this->path.push_back(this->id_init);
+    this->is_generated = false;
     print();
   }
   Map()
-  : goal(4,4), init(0,0), curr(0,0) {
-    this->size = 9;
+  : goal_(1,1), init_(0,0), curr_(0,0), graph(9) {
+    this->size_ = 3;
+    this->id_goal = this->size_ * this->goal_.getX() + this->goal_.getY();
+    this->id_init = this->size_ * this->init_.getX() + this->init_.getY();
+    this->id_curr = this->size_ * this->curr_.getX() + this->curr_.getY();
+    this->path.push_back(this->id_init);
+    this->is_generated = false;
     print();
   }
   void print() {
-    std::cout << " Goal:" << goal.getX() << ", " << goal.getY()
-    << " Init:" << init.getX() << ", " << init.getY()
-    << " Curr:" << curr.getX() << ", " << curr.getY() << std::endl;
+    std::cout << " Goal:" << this->goal_.getX() << ", " << this->goal_.getY()
+    << " Init:" << this->init_.getX() << ", " << this->init_.getY()
+    << " Curr:" << this->curr_.getX() << ", " << this->curr_.getY() << std::endl;
   }
-  void generateMap() {
-    std::cout << "gen " << size << std::endl;
-    for(int i=0; i<size; i++){
-      for(int j=0; j<size; j++){
-        // std::cout << "n " << (i+1)*(j+1) << std::endl;
-        int id = size*i + j;
-        nodes.push_back(Node(id, i, j, goal));
-      }
-    }
-  }
-
   void generateGraph(){
+    if (this->is_generated){
+      return;
+    }
     int id = 0;
-    for(int i=0; i<this->size; i++){
-      for(int j=0; j<this->size; j++){
-        id = size*i + j;
+    for(int i=0; i<this->size_; i++){
+      for(int j=0; j<this->size_; j++){
+        id = this->size_ * i + j;
         // std::cout << "n:" << size << " id:" << id << " i:" << i << " j:" << j << std::endl;
-        nodes.push_back(Node(id, i, j, goal));
+        Node* n = new Node(id, i, j, this->goal_);
+        nodes[id] = n;
         if (j > 0) {
           this->graph.addEdge(id-1, id);
         }
-        if (j < size-1) {
+        if (j < this->size_-1) {
           this->graph.addEdge(id, id+1);
         }
         if (i > 0) {
-          this->graph.addEdge(id-size, id);
+          this->graph.addEdge(id-this->size_, id);
         }
-        if (i < size-1) {
-          this->graph.addEdge(id, id+size);
+        if (i < this->size_-1) {
+          this->graph.addEdge(id, id+this->size_);
         }
         // this->graph.printGraph();
       }
     }
+    nodes[id_init]->setG(0.0);
+    this->is_generated = true;
   }
+  std::map<int, Node*> getNodes() { return nodes; }
+  Graph* getGraph() { return &graph; }
+  void printAdj() { this->graph.printGraph(); }
+  void viewGraph() { 
+    for(int i=this->size_-1; i>=0; i--){
+      for(int j=0; j<this->size_; j++){
+        int id = this->size_ * i + j;
+        if (std::find(path.begin(), path.end(), id) != path.end()){
+          std::cout << "[ *] ";
+        } else {
+          std::cout << '[' << std::setw(2) << id << "] ";
+        }
+      }
+      std::cout << std::endl;
+      std::cout << std::endl;
+    }
+  }
+  int solveNextStep(int n) {
+    // get current node g(n) + 1 for next step
+    double g_n = this->nodes[n]->getG() + 1;
+    // initialize min value
+    double min_f = 1000.0;
+    Node* n_min = NULL;
 
-  std::vector<Node> getNodes() { return nodes; };
-  
-  void viewGraph() { this->graph.printGraph(); };
+    std::cout << std::setprecision(2) << std::fixed;
+    // iterate through the node neighbors
+    for(int &i: this->graph.getEdges(id_curr)){
+      Node* n_temp = this->nodes[i];
+      // update actual cost g(n)
+      n_temp->setG(g_n);
+      std::cout << "  node[" << std::setw(2) << i
+      << "] h=" << std::setw(5) << n_temp->getH()
+      << " f=" << std::setw(5) << n_temp->getF() << std::endl;
+      // if lower than min, update min f(n) & index
+      if(n_temp->getF() < min_f){
+        min_f = n_temp->getF();
+        n_min = n_temp;
+      }
+    }
+    std::cout << "* node[" << std::setw(2) << n_min->getId()
+    << "] h(n)=" << std::setw(5) << n_min->getH()
+    << " min f(n)=" << std::setw(5) << n_min->getF()
+    << std::endl << std::endl;
+    // return node index with the lowest f(n)
+    return n_min->getId();
+  }
+  void solveMap() {
+    std::cout << "goal[" << this->id_goal << "]" << std::endl;
+    // loop until current node is goal node
+    while(this->id_curr != this->id_goal) {
+      this->id_curr = this->solveNextStep(this->id_curr);
+      // add next node to path
+      path.push_back(this->id_curr);
+    }
+  }
 };
 
 class Explorer{
